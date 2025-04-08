7c150c182b3cfbaa4a8273312474418137d2712a
elio.heinz <elioheinz2004@gmail.com>
2025-04-08 11:28:28 +0200

    aufgabe 1 done insallah

--- src/ch/hslu/prg/ledmuster/g1_09/ClientApp.java
+++ src/ch/hslu/prg/ledmuster/g1_09/ClientApp.java
@@ -1,9 +1,6 @@
 package ch.hslu.prg.ledmuster.g1_09;
 import ch.hslu.prg.ledboard.proxy.BoardService;
 import ch.hslu.prg.ledboard.proxy.Led;
-import ch.hslu.prg.ledboard.proxy.LedColor;
-
-import java.security.Provider.Service;
 import java.util.Scanner;
 
 public class ClientApp {
@@ -31,33 +28,41 @@ public class ClientApp {
 			boardService.add(ledRows); // LED Rows werden an Board gegeben (Aufgabe 1.1.2)
 			// Alle LEDS ins Array einlesen
 			Led[][] allLeds = boardService.getAllLeds();
-			// LEDs von rechts nach links einschalten (Aufgabe 1.1.4)
-			for (int row = allLeds.length - 1; row >= 0; row--) {
-				for (int column = allLeds[row].length - 1; column >= 0; column--) {
-					// Einzelnes LED nehmen und einschalten
-					Led currentLED = allLeds[row][column];
-					currentLED.turnOn();
-					
-					// Kurz Pausieren damit es ersichtlich wird
-					boardService.pauseExecution(50); 
-				}	
+			
+			for (int i = 0; i < 3; i++) { // 3 Mal ausführen (Aufgabe 1.1.8)
+				
+				// LEDs von rechts nach links einschalten (Aufgabe 1.1.4)
+				for (int row = allLeds.length - 1; row >= 0; row--) {
+					for (int column = allLeds[row].length - 1; column >= 0; column--) {
+						// Einzelnes LED nehmen und einschalten
+						Led currentLED = allLeds[row][column];
+						currentLED.turnOn();
+						
+						// Kurz Pausieren damit es ersichtlich wird
+						boardService.pauseExecution(10); 
+					}	
+				}
+				
+				boardService.pauseExecution(250); // (Aufgabe 1.1.5)
+				
+				// Einzelnes LED nehmen und ausschalten (Aufgabe 1.1.6)
+				for (int row = 0; row <= allLeds.length - 1; row++) {
+					for (int column = 0;  column <= allLeds[row].length - 1; column++) {
+						Led currentLED = allLeds[row][column];
+						currentLED.turnOff();
+						
+						// Kurz Pausieren damit es ersichtlich wird
+						boardService.pauseExecution(10);
+					}	
+				}
+				
+				boardService.pauseExecution(250); // (Aufgabe 1.1.7)
 			}
 			
-			boardService.pauseExecution(250); // (Aufgabe 1.1.5)
+			boardService.pauseExecution(2000); // 2 Sekunden warten (Aufgabe 1.1.9)
 			
-			// Einzelnes LED nehmen und ausschalten (Aufgabe 1.1.6)
-			for (int row = 0; row <= allLeds.length - 1; row++) {
-				for (int column = 0;  column <= allLeds[row].length - 1; column++) {
-					Led currentLED = allLeds[row][column];
-					currentLED.turnOff();
-					
-					// Kurz Pausieren damit es ersichtlich wird
-					boardService.pauseExecution(50);
-				}	
-			}
+			boardService.removeAllLeds(); // Leds Zurücksetzen (Aufgabe 1.1.10)			
 			
-			boardService.pauseExecution(250); // (Aufgabe 1.1.7)
-
 		} else {
 			System.out.println("Die Anzahl LED Reihen ist nicht gültig");
 			return; 
