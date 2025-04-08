06f18a803e0cf7fd567ab6e9f804f9335e885143
elio.heinz <elioheinz2004@gmail.com>
2025-04-08 11:20:04 +0200

    neue aufgaben bis 1.1.7

--- src/ch/hslu/prg/ledmuster/g1_09/ClientApp.java
+++ src/ch/hslu/prg/ledmuster/g1_09/ClientApp.java
@@ -1,30 +1,63 @@
 package ch.hslu.prg.ledmuster.g1_09;
 import ch.hslu.prg.ledboard.proxy.BoardService;
+import ch.hslu.prg.ledboard.proxy.Led;
+import ch.hslu.prg.ledboard.proxy.LedColor;
+
+import java.security.Provider.Service;
 import java.util.Scanner;
 
 public class ClientApp {
 
 	public static void main(String[] args) {
-		ledsOnOff();	
+		BoardService boardService = new BoardService(); //Neues Objekt für BoardService
+		// LedColor ledColor = new LedColor(); // Neues Objekt für ledColors
+
+		ledsOnOff(boardService);	//Mehtode ledsOnOff aufrufen und service mitgeben
 	}
 	
-	private static void ledsOnOff() {	
-		int ledRows = 0;
-		BoardService service = new BoardService(); //Neues Objekt für BoardService
+	private static void ledsOnOff(BoardService boardService) {	
+		boardService.pauseExecution(2000); // 2 Sekunden warten (Aufgabe 1.1.3)
+		
 		Scanner sc = new Scanner(System.in);
 		
-		// Einlesen von  LED Rows
+		// Rows definieren
+		int ledRows = 0;
+		
+		// Einlesen von LED rows (Aufgabe 1.1.1)
 		System.out.println("Anzahl LED Reihen(1-32): ");
 		ledRows = sc.nextInt();
 		
-		// Input Validation
-		if(ledRows <= BoardService.MAX_ROWS && ledRows > 0) {
-			service.add(ledRows);
-			System.out.println("es fängt an ");
-			service.pauseExecution(2000);
-			System.out.println("es hört auf");
-
+		if(ledRows <= BoardService.MAX_ROWS && ledRows > 0) { // Input Validation (Aufgabe 1.1.1)
+			boardService.add(ledRows); // LED Rows werden an Board gegeben (Aufgabe 1.1.2)
+			// Alle LEDS ins Array einlesen
+			Led[][] allLeds = boardService.getAllLeds();
+			// LEDs von rechts nach links einschalten (Aufgabe 1.1.4)
+			for (int row = allLeds.length - 1; row >= 0; row--) {
+				for (int column = allLeds[row].length - 1; column >= 0; column--) {
+					// Einzelnes LED nehmen und einschalten
+					Led currentLED = allLeds[row][column];
+					currentLED.turnOn();
+					
+					// Kurz Pausieren damit es ersichtlich wird
+					boardService.pauseExecution(50); 
+				}	
+			}
 			
+			boardService.pauseExecution(250); // (Aufgabe 1.1.5)
+			
+			// Einzelnes LED nehmen und ausschalten (Aufgabe 1.1.6)
+			for (int row = 0; row >= allLeds.length - 1; row++) {
+				for (int column = 0;  column >= allLeds[row].length - 1; column++) {
+					Led currentLED = allLeds[row][column];
+					currentLED.turnOff();
+					
+					// Kurz Pausieren damit es ersichtlich wird
+					boardService.pauseExecution(50);
+				}	
+			}
+			
+			boardService.pauseExecution(250); // (Aufgabe 1.1.7)
+
 		} else {
 			System.out.println("Die Anzahl LED Reihen ist nicht gültig");
 			return; 
